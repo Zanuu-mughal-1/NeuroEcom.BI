ba56515a7f76b4b4c65130a2f414aea64fd23a8c
Zanuu-mughal-1 <yttech4u2022@gmail.com>
2026-05-01 22:41:59 +0500

    Start Powershell Added

---
+++ Start.ps1
@@ -0,0 +1,5 @@
+# Open Backend in a new window
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd backend; dotnet run"
+
+# Open Frontend in a new window
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd frontend; npm run dev"
